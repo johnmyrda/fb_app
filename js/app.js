dfdb63d632bcc999dc33d61f5a273f46ed6e8f7c
John Myrda <johnmyrda@gmail.com>
2014-12-08 22:18:27 -0800

    Library Initialization moved to lib.js. History is not started until the StateModel returns the loginStatus in order to avoid misfiring of the Router.

--- js/app.js
+++ js/app.js
@@ -1,31 +1,11 @@
-define(["jquery", "keys", "parse", "facebook",
+define(["parse",
          "models/state_model", "router"],
-function($, AppKeys, Parse, FB, StateModel, AppRouter) {
-    (function () {
-
+function(Parse, StateModel, AppRouter) {
         "use strict";
 
-
-        //Initialize Parse SDK with JavaScript keys
-        Parse.initialize(AppKeys.parse.appId, AppKeys.parse.jsKey);
-
-        //Initialize Facebook SDK
-        Parse.FacebookUtils.init({
-            appId: AppKeys.facebook.appId, // App ID
-            xfbml: false, // parse XFBML
-            version: 'v2.1'
-        });
-
         //Start the App
-        FB.getLoginStatus(function (response) {
-            if (response.status === "connected" && Parse.User.current() && Parse.User.current().get("authData").facebook.id === response.authResponse.userID) {
-                StateModel.set("loginStatus", true);
-            } else {
-                StateModel.logOut();
-            }
-            new AppRouter();
+        new AppRouter();
+        StateModel.determineLoginStatus(function(){
             Parse.history.start({pushState: true});
         });
-
-    })();
 });

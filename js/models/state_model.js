7eadeb013b7946da199c075736cd5c2091235a4f
John Myrda <johnmyrda@gmail.com>
2014-12-08 22:21:15 -0800

    StateModel now uses FB API to check the User's Login Status. Flag variable "init" added to notify app when it is safe to start.

--- js/models/state_model.js
+++ js/models/state_model.js
@@ -1,17 +1,44 @@
 define([
-    'parse'
+    'parse',
+    'facebook'
 ], function(){
     var StateModel = Parse.Object.extend("AppState", {
 
         defaults: {
             loginStatus: false //True if user is logged in, false if not
+            ,init: false
+        },
+
+        init: function(){
+        },
+
+        determineLoginStatus: function (callback, options){
+            var self = this;
+            //FB.getLoginStatus is Async. Be Careful.
+            FB.getLoginStatus(function (response) {
+                console.log("fbbs");
+                if (response.status === "connected" && Parse.User.current() && Parse.User.current().get("authData").facebook.id === response.authResponse.userID) {
+                    self.set("loginStatus", true);
+                } else {
+                    self.logOut();
+                }
+                if(callback && typeof(callback) === "function"){
+                    if(!options){
+                        options = {};
+                    }
+                    //console.log(callback, options);
+                    callback();
+                }
+            });
         },
 
         logOut: function () {
-            console.log(this.get("loginStatus"));
+            //console.log(this.get("loginStatus"));
             Parse.User.logOut();
             this.set({"loginStatus": false});
         }
+    },{//classProps
+
     });
 
     return new StateModel();
